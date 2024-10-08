43f69cdcb415abb800859ce9117b7bd52a954fc3
Everton Bitencourt <everton.silvaa@gmail.com>
2024-10-08 14:56:53 -0300

    Implementando a lógica do cadastro, lógica da listagem de cadastros

--- src/main/java/com/senacead/pilotos/controller/PilotoController.java
+++ src/main/java/com/senacead/pilotos/controller/PilotoController.java
@@ -2,12 +2,18 @@
 package com.senacead.pilotos.controller;
 
 import com.senacead.pilotos.model.Piloto;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PilotoController {
+    
+    private List<Piloto> listaPilotos = new ArrayList();
     
     @GetMapping("/")
     public String inicio(){
@@ -25,8 +31,17 @@ public class PilotoController {
         return "cadastro";
     }
     
+    @PostMapping("/guardar-piloto")
+    public String processarformulario(Model model, @ModelAttribute Piloto piloto){
+        piloto.setId(listaPilotos.size()+1);
+        piloto.setStatus(true);
+        listaPilotos.add(piloto);
+        return "redirect:/listagem";
+    }
+    
     @GetMapping("/listagem")
-    public String listar(){
+    public String listar(Model model){
+        model.addAttribute("pilotos", listaPilotos);
         return "listagem";
     }
 }
